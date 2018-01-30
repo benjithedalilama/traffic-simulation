4e6a06c05a7bcae63978d704fe4f2b3d06a7d468
Ben Imadali <benjithedalilama@users.noreply.github.com>
2018-01-30 09:45:43 +0530

    changed some variable names for clarity

--- TrafficSimulation.py
+++ TrafficSimulation.py
@@ -20,7 +20,7 @@ class TrafficSimulation():
 
     def step(self):
         length = self.road_length
-        temp_state = deepcopy(self.state)
+        speeds = deepcopy(self.state)
         for index, speed in enumerate(self.state):
             if speed < 0:
                 continue
@@ -32,33 +32,33 @@ class TrafficSimulation():
 
             # accelerate, or slow down due to cars
             if speed < self.v_max and (j > speed + 1 or self.state[(index+j)%length] < 0):
-                temp_state[index] += 1
+                speeds[index] += 1
             elif j <= speed:
-                temp_state[index] = j - 1
+                speeds[index] = j - 1
 
             # random slow down
-            if temp_state[index] > 0 and random() < self.p:
-                temp_state[index] -= 1
+            if speeds[index] > 0 and random() < self.p:
+                speeds[index] -= 1
 
         # update the positions
-        temp_advancement_state = [-1]*length
-        for index, speed in enumerate(temp_state):
+        updated_state = [-1]*length
+        for index, speed in enumerate(speeds):
             if speed < 0:
                 continue
-            temp_advancement_state[(index + speed)%length] = speed
+            updated_state[(index + speed)%length] = speed
 
         # peer into the future to slow down, but dont change position
-        for index, speed in enumerate(temp_advancement_state):
+        for index, speed in enumerate(updated_state):
             if speed < 0:
                 continue
             j = 1
-            while temp_advancement_state[(index + j)%length] < 0:
+            while updated_state[(index + j)%length] < 0:
                 j += 1
             if j <= speed:
-                temp_advancement_state[index] = j - 1
+                updated_state[index] = j - 1
 
         # iterate the state, and please stop the hate
-        self.state = temp_advancement_state
+        self.state = updated_state
 
     def display(self):
         print(''.join('.' if x == -1 else str(x) for x in self.state))
