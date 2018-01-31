efddf50a8d652feda5366124d5ee2bdf5b159b77
Ben Imadali <benjithedalilama@users.noreply.github.com>
2018-01-31 17:32:44 +0530

    added flow analysis, graph functionality added for density vs. flow plot, still deciding on which regression model is best

--- TrafficSimulation.py
+++ TrafficSimulation.py
@@ -1,17 +1,20 @@
-from random import random
+import random
 from copy import deepcopy
 
 class TrafficSimulation():
-    def __init__(self, road_length, traffic_density, v_max, p, start_state = []):
+    def __init__(self, road_length, traffic_density, v_max, p, start_state = [], verbose = True):
         self.road_length = road_length
         self.traffic_density = traffic_density
         self.v_max = v_max
         self.p = p
+        self.verbose = verbose
+        self.throughput = 0
         if not start_state:
             self.state = []
             for i in range(road_length):
-                if random() < traffic_density:
-                    self.state.append(v_max//2)
+                if random.random() < traffic_density:
+                    rand_init_speed = random.choice(range(v_max))
+                    self.state.append(rand_init_speed)
                 else:
                     self.state.append(-1)
         else:
@@ -21,6 +24,7 @@ class TrafficSimulation():
     def step(self):
         length = self.road_length
         speeds = deepcopy(self.state)
+
         for index, speed in enumerate(self.state):
             if speed < 0:
                 continue
@@ -39,10 +43,11 @@ class TrafficSimulation():
                 speeds[index] = j - 1
 
             # random slow down
-            if speeds[index] > 0 and random() < self.p:
+            if speeds[index] > 0 and random.random() < self.p:
                 speeds[index] -= 1
 
-        display(speeds)
+        if self.verbose:
+            self.display(speeds)
 
         # update the positions
         updated_state = [-1]*length
@@ -50,9 +55,15 @@ class TrafficSimulation():
             if speed < 0:
                 continue
             updated_state[(index + speed)%length] = speed
+            self.throughput += float(speed)
 
         # iterate the state, and please stop the hate
         self.state = updated_state
 
-def display(state):
-    print(''.join('.' if x == -1 else str(x) for x in state))
+    def get_flow(self):
+        # distance covered by cars divided by number of cars
+        flow = self.throughput / self.road_length
+        return flow
+
+    def display(state):
+        print(''.join('.' if x == -1 else str(x) for x in state))
--- example.py
+++ example.py
@@ -1,26 +1,38 @@
 from trafficsimulation import TrafficSimulation
-import os
-import time
+# import seaborn as sns
+import numpy as np
+# import pandas as pd
+import matplotlib.pyplot as plt
 
-# these are for a very simple animation
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
-def pause():
-    time.sleep(0.1)
 
-# not animated
-sim = TrafficSimulation(100, 0.1, 5, 0.5)
-for i in range(40):
-    sim.step()
-    # sim.display()
-#
-# # animated
-# sim = TrafficSimulation(100, 0.1, 5, 0.5)
-# sim.display()
-# for i in range(40):
-#     pause()
-#     cls()
-#     sim.step()
-#     sim.display()
-#
-# sim = TrafficSimulation()
+def get_flow_array(densities, iterations):
+    avg_flow_arr = []
+    for density in densities:
+        sim = TrafficSimulation(100,density,5,.5, verbose=False)
+        flow_sum = 0
+        for i in range(iterations):
+            sim.step()
+            flow = sim.get_flow()
+            flow_sum += flow
+        avg_flow = flow_sum/(iterations*sim.road_length)
+        avg_flow_arr.append(avg_flow)
+    return avg_flow_arr
+
+dens = np.arange(0.01,1.0,.01)
+its = 200
+
+flows = get_flow_array(dens, its)
+dens = np.array(dens)
+flows = np.array(flows)
+# df = pd.DataFrame({'density':dens, 'flow':flows})
+# ax = sns.lmplot(x='density', y='flow', fit_reg=True, data=df)
+plt.scatter(dens,flows,s=2)
+z = np.polyfit(dens, flows, 3)
+f = np.poly1d(z)
+plt.plot(dens, f(dens))
+plt.xlabel('density (cars per cell)')
+plt.ylabel('Flow (cars per time-step)')
+plt.title('Flow vs. Density')
+plt.grid(True)
+plt.savefig("test.png")
+raise Exception
