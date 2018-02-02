fd15fbf6286536258d59cfa0958bded77a2121e1
Ben Imadali <benjithedalilama@users.noreply.github.com>
2018-02-02 10:19:46 +0530

    thought I could make it go fast, but for some reason i cant

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from trafficsimulation import TrafficSimulation
+import subprocess
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.kernel_ridge import KernelRidge
+from joblib import Parallel, delayed
 
 def get_flow_array(densities, iterations, strategy):
     avg_flow_arr = []
@@ -19,19 +21,31 @@ def get_flow_array(densities, iterations, strategy):
         avg_flow_arr.append(avg_flow)
     return avg_flow_arr
 
-dens = np.repeat(np.arange(0.0,1.0,.005),5)
+dens = np.repeat(np.arange(0.0,1.0,.03),5)
 its = 200
 strategies = ['regular','middle']
 train_size = len(dens)
 for strategy in strategies:
-    flows = get_flow_array(dens, its, strategy)
+    strat_to_be_zipped = ['strategy']*len(dens)
+    its_to_be_zipped = [its]*len(dens)
+    arg_instances = zip(dens, its_to_be_zipped, strat_to_be_zipped)
+    start = time.time()
+    flows = Parallel(n_jobs=4, backend="multiprocessing")(delayed(get_flow_array)(densities = dens[i:i+10], iterations = its, strategy = strategy) for i in range(0,len(dens),10))
+    elapsed = time.time() - start
     flows = np.array(flows)
+    subprocess.run(['say','Hello! %s Strategy is done parallelized in %d seconds'%(strategy,elapsed)])
+    flows.flatten()
+    start2 = time.time()
+    flows = get_flow_array(dens, its, strategy)
+    elapsed2 = time.time() - start
+    flows = np.array(flows)
+    subprocess.run(['say','Hello! %s Strategy is done regularly in %d seconds'%(strategy,elapsed)])
     plt.scatter(dens,flows,s=2,label='%s'%strategy)
     kr = GridSearchCV(KernelRidge(kernel='chi2', gamma=0.1), cv=5,
                   param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3],
                               "gamma": np.logspace(-2, 2, 5)})
     dens = dens.reshape(len(dens),1)
-    flows = flows.reshape(len(flows),1)
+    flows = flows.reshape(len(dens),1)
     kr.fit(dens[:train_size], flows[:train_size])
     y_kr = kr.predict(dens)
     color = 'r' if strategy == 'regular' else 'g'
